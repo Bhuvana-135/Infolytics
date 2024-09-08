d0433a6174976e129055b1206fba07faf2a0c856
Bhuvana-135 <132888994+Bhuvana-135@users.noreply.github.com>
2024-09-08 13:39:56 +0530

    Add files via upload

---
+++ Help/Infolytics/make.bat
@@ -0,0 +1,42 @@
+@ECHO ON
+
+pushd %~dp0
+
+REM Command file for Sphinx documentation
+if "%SPHINXBUILD%" == "" (
+	set SPHINXBUILD=sphinx-build
+)
+
+set SOURCEDIR=source
+set BUILDROOT=..
+set BUILDDIR="%BUILDROOT%/_build/infolytics"
+set ALLSPHINXOPTS=-a -d "%BUILDDIR%/doctrees" %SPHINXOPTS% -c "%BUILDROOT%/" "%BUILDROOT%"
+set I18NSPHINXOPTS=%SPHINXOPTS% "%BUILDROOT%"
+if NOT "%PAPER%" == "" (
+	set ALLSPHINXOPTS=-D latex_paper_size=%PAPER% %ALLSPHINXOPTS%
+	set I18NSPHINXOPTS=-D latex_paper_size=%PAPER% %I18NSPHINXOPTS%
+)
+
+if "%1" == "" goto help
+
+%SPHINXBUILD% >NUL 2>NUL
+if errorlevel 9009 (
+	echo.
+	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
+	echo.installed, then set the SPHINXBUILD environment variable to point
+	echo.to the full path of the 'sphinx-build' executable. Alternatively you
+	echo.may add the Sphinx directory to PATH.
+	echo.
+	echo.If you don't have Sphinx installed, grab it from
+	echo.http://sphinx-doc.org/
+	exit /b 1
+)
+
+%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
+goto end
+
+:help
+%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
+
+:end
+popd
