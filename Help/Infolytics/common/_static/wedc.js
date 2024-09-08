bc0fb8606afcf3a33dc28a42b5e84cc556a58ada
Bhuvana-135 <132888994+Bhuvana-135@users.noreply.github.com>
2024-09-08 13:56:19 +0530

    Add files via upload
    all files under static folder.

---
+++ Help/Infolytics/common/_static/wedc.js
@@ -0,0 +1,9 @@
+var varSegmentation = 0;
+var varClickTracking = 1;
+var varCustomerTracking = 1;
+var varAutoFirePV = 1;
+var Route = 12656;
+var Ctrl = "";
+
+document.write("<script type='text/javascript' src='_static/svg-pan-zoom.js'><\/script>");
+document.write("<script>" + "window.onload = function() { var hasSVG = document.getElementsByTagName('svg').length > 0; if( hasSVG ) window.zoomTiger = svgPanZoom('svg', { zoomEnabled: true, controlIconsEnabled: true, fit: true, center: true,});};" + "<\/script>");
